a659a261f3f9a0d4a7f6ab7cad8d2387c5a9c770
Steffen <steven@posteo.de>
2026-03-20 18:06:06 -0400

    Cleanup

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -78,12 +78,6 @@
         # the tradeoff is increased power consumption.
         FastConnectable = false;
       };
-      Policy = {
-        # Enable all controllers when they are found. This includes
-        # adapters present on start as well as adapters that are plugged
-        # in later on. Defaults to 'true'.
-        AutoEnable = true;
-      };
     };
   };
 
